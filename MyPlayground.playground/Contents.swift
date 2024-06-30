f67dedbaa0ff427550432dfc47965f6024fe55f0
freak-circus <77841607+freak-circus@users.noreply.github.com>
2024-07-01 00:03:57 +0300

    вынес мусор, повысил читаемость и исправил задания 2 и 5

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,10 +1,11 @@
+//1
 class Car {
-    var Brand : String
-    var Model : String
-    var ManufactureYear : Int
-    var Cost : Double
+    var Brand: String
+    var Model: String
+    var ManufactureYear: Int
+    var Cost: Double
     
-    init(Brand : String, Model : String, ManufactureYear : Int, Cost : Double){
+    init(Brand: String, Model: String, ManufactureYear: Int, Cost: Double){
         self.Brand = Brand
         self.Model = Model
         self.ManufactureYear = ManufactureYear
@@ -18,46 +19,83 @@ class Car {
         return "\(self.Brand) \(self.Model)"
     }
 }
-//
-class Crossover : Car {
-    var groundСlearance : Double = 1
+//2
+class Crossover: Car {
+    var groundClearance: Double
+
+    init(Brand: String, Model: String, ManufactureYear: Int, Cost: Double, groundClearance: Double) {
+        self.groundClearance = groundClearance
+        super.init(Brand: Brand, Model: Model, ManufactureYear: ManufactureYear, Cost: Cost)
+    }
 }
 
-class Sedan : Car {
-    var luxury : Bool = false
+class Sedan: Car {
+    var luxury: Bool
+
+    init(Brand: String, Model: String, ManufactureYear: Int, Cost: Double, luxury: Bool) {
+        self.luxury = luxury
+        super.init(Brand: Brand, Model: Model, ManufactureYear: ManufactureYear, Cost: Cost)
+    }
 }
 
-class Waggon : Car {
-    var baggageVolume:Int = 1
-}
-class Hatchback : Car {
-    var weight:Int = 1
-}
-//
-func ManufactureCar(brand : String, model:String, manufactureYear:Int, cost:Double) -> Car {
-    return Car(Brand:brand, Model:model, ManufactureYear:manufactureYear, Cost:cost)
+class Waggon: Car {
+    var baggageVolume: Int
+
+    init(Brand: String, Model: String, ManufactureYear: Int, Cost: Double, baggageVolume: Int) {
+        self.baggageVolume = baggageVolume
+        super.init(Brand: Brand, Model: Model, ManufactureYear: ManufactureYear, Cost: Cost)
+    }
 }
 
-//
-func makeRun(firstCar : Car, secondCar : Car) -> Car{
+class Hatchback: Car {
+    var weight: Int
+
+    init(Brand: String, Model: String, ManufactureYear: Int, Cost: Double, weight: Int) {
+        self.weight = weight
+        super.init(Brand: Brand, Model: Model, ManufactureYear: ManufactureYear, Cost: Cost)
+    }
+}
+
+//3
+func ManufactureCar(brand: String, model: String, manufactureYear: Int, cost:Double) -> Car {
+    return Car(Brand: brand, Model: model, ManufactureYear: manufactureYear, Cost: cost)
+}
+
+//4
+func makeRun(firstCar: Car, secondCar: Car) -> Car{
     var winner = firstCar.Cost >= secondCar.Cost ? firstCar : secondCar
     return winner
 }
 
-//
-var cars : [Car] = []
+//5
+var cars: [Car] = []
 cars.append(ManufactureCar(brand: "vw", model: "golf", manufactureYear: 2018, cost: 22854))
 cars.append(ManufactureCar(brand: "skoda", model: "octavia", manufactureYear: 2012, cost: 12222))
 cars.append(ManufactureCar(brand: "seat", model: "leon", manufactureYear: 2007, cost: 8635))
 cars.append(ManufactureCar(brand: "audi", model: "a3", manufactureYear: 2022, cost: 31098))
 
-while cars.count > 1{
-    for i in 1..<cars.count/2{
-        var winner = makeRun(firstCar: cars[i-1-i+1], secondCar: cars[i*2-1-i+1])
-        print("\(winner.GetCarInfo()) победил в долларовой схватке против \(cars[i*2-1].GetCarInfo())")
-        cars.remove(at: i*2-1-i+1)
+var statistics: Dictionary<String, Int> = [:]
+for car in cars {
+    statistics[car.GetCarInfo()] = 0
+    for opponent in cars{
+        if car.Cost.isEqual(to: opponent.Cost){
+            continue
+        }
+        if car.Cost > opponent.Cost{
+            statistics[car.GetCarInfo()]! += 1
+            print("\(car.GetCarInfo()) одержал победу над \(opponent.GetCarInfo())")
+        }
     }
 }
 
-print("Победитель")
-cars[0].PrintCarInfo()
+
+var maxwins: Int = 0
+var winner: String = ""
+for (key, value) in statistics {
+    if value > maxwins {
+        maxwins = value
+        winner = key
+    }
+}
+
+print("По итогам всех встреч первое место занимает \(winner) с числом побед: \(maxwins)")
